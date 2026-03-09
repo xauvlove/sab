a5285fba09b7a7ae90143eed31764ea95f9fd9d4
SleepStory Developer <developer@sleepstory.app>
2026-03-09 21:35:09 +0800

    1

--- SleepStoryBackend/src/main/java/com/sleepstory/backend/SleepStoryApplication.java
+++ SleepStoryBackend/src/main/java/com/sleepstory/backend/SleepStoryApplication.java
@@ -1,5 +1,6 @@
 package com.sleepstory.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@MapperScan("com.sleepstory.backend.dal.mapper")
 public class SleepStoryApplication {
 
     public static void main(String[] args) {
